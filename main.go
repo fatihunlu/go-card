e18a9cad50e57e496954d1bcafafd91cef5b8c95
Fatih Ünlü <unlu.fa@gmail.com>
2021-12-27 16:16:04 +0300

    add deck.go

--- main.go
+++ main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world..")
+	cards := newDeck()
+	hand, remainingCards := deal(cards, 3)
+
+	hand.print()
+	fmt.Println("------")
+	remainingCards.print()
+
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
 }
